077e024f9cb76445ac77136e12d0335695ba453c
Lima-Almeida <m242827@dac.unicamp.br>
2023-08-30 11:34:29 -0300

    dataset download function and pandas update

--- housing_func.py
+++ housing_func.py
@@ -1,5 +1,6 @@
 import os
 import tarfile
+import pandas as pd
 from six.moves import urllib
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master"
@@ -15,3 +16,8 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)

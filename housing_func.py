4abe5e88b163185cf980eff7babd707696517f52
Lima-Almeida <m242827@dac.unicamp.br>
2023-08-30 20:18:00 -0300

    Comments and test set function usage example

--- housing_func.py
+++ housing_func.py
@@ -8,6 +8,8 @@ DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 
+
+#This function downloads the housing dataset
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     if not os.path.isdir(housing_path):
         os.makedirs(housing_path)
@@ -24,9 +26,15 @@ def load_housing_data(housing_path=HOUSING_PATH):
     return pd.read_csv(csv_path)
 
 
+#This function creates a test set randomly -> not recommended for the housing set due to the low quantity of values (may introduce bias)
 def split_train_test(data, test_ratio):
     shuffled_indices = np.random.permutation(len(data))
     test_set_size = int(len(data) * test_ratio)
     test_indices = shuffled_indices[:test_set_size]
     train_indices = shuffled_indices[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
+
+
+#Function usage example
+#train_set, test_set = split_train_test(housing, 0.2)
+#print(len(train_set), "train +", len(test_set), "test")
